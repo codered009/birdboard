2c65ce31ff83c0ef10e17c513bd5c6a263ddde11
codered009 <48705158+codered009@users.noreply.github.com>
2019-09-30 02:13:37 +0530

    Update 2018_12_17_150648_create_projects_table.php

--- database/migrations/2018_12_17_150648_create_projects_table.php
+++ database/migrations/2018_12_17_150648_create_projects_table.php
@@ -14,6 +14,7 @@ class CreateProjectsTable extends Migration
     public function up()
     {
         Schema::create('projects', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
             $table->bigIncrements('id');
             $table->unsignedBigInteger('owner_id');
             $table->string('title');
