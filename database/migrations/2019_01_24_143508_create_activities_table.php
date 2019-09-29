c25ccc942a246b6c31884dc451a1d54c9fc5c937
codered009 <48705158+codered009@users.noreply.github.com>
2019-09-30 02:14:48 +0530

    Update 2019_01_24_143508_create_activities_table.php

--- database/migrations/2019_01_24_143508_create_activities_table.php
+++ database/migrations/2019_01_24_143508_create_activities_table.php
@@ -14,6 +14,7 @@ class CreateActivitiesTable extends Migration
     public function up()
     {
         Schema::create('activities', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
             $table->increments('id');
             $table->unsignedInteger('user_id');
             $table->unsignedInteger('project_id');
